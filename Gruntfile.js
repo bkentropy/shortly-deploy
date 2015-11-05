74d124b0d8cd52084c66e5e89188963192bdab0a
Brian Kustra <bkustrakb@gmail.com>
2015-11-05 12:44:30 -0600

    Changed route to dist. Added test files to Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+      
     concat: {
       dist: {
         src: ['public/client/*.js'],
@@ -111,7 +112,7 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [ 'concat: dist', 'uglify: dist']);
+  grunt.registerTask('build', [ 'concat', 'uglify' ]);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.configure(function() {
     app.set('view engine', 'ejs');
     app.use(partials());
     app.use(express.bodyParser());
-    app.use(express.static(__dirname + '/public'));
+    //app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/dist'));
     app.use(express.cookieParser('shhhh, very secret'));
     app.use(express.session());
 });
